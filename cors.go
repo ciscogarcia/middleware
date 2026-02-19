ade570e7fa5f78d0d27061c020793493e731f403
Francisco Garcia <fgarc253@ford.com>
2026-02-19 15:24:21 -0500

    Add CORS for http.Handler

--- cors.go
+++ cors.go
@@ -16,3 +16,21 @@ func CORS(next http.HandlerFunc) http.HandlerFunc {
 		next(w, r)
 	}
 }
+
+// corsMiddleware adds CORS headers to the response
+func CORSHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set the desired CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Allows all origins
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		// Handle preflight OPTIONS requests
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		// Pass the request to the next handler in the chain (http.FileServer)
+		next.ServeHTTP(w, r)
+	})
+}
