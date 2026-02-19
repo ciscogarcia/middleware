922df68eeb9aba3b9445dc20cd789d7c1698aecc
Francisco Garcia <fgarc253@ford.com>
2026-02-18 20:44:09 -0500

    Remove OPTIONS check
    Remove OPTIONS Check in attempt to allow PUT

--- cors.go
+++ cors.go
@@ -9,10 +9,10 @@ func CORS(next http.HandlerFunc) http.HandlerFunc {
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 
-		if r.Method == "OPTIONS" {
-			http.Error(w, "No Content", http.StatusNoContent)
-			return
-		}
+		// if r.Method == "OPTIONS" {
+		// 	http.Error(w, "No Content", http.StatusNoContent)
+		// 	return
+		// }
 
 		next(w, r)
 	}
