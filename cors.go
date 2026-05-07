0b3b96f46cd3c874716536150c99e3ff96812d99
Francisco Garcia <fgarc253@ford.com>
2026-05-07 10:28:44 -0400

    add inpro origin

--- cors.go
+++ cors.go
@@ -19,7 +19,7 @@ func CORS(next http.HandlerFunc) http.HandlerFunc {
 
 func CORSReact(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Add("Access-Control-Allow-Origin", "https://timecard.inproenterprises.org")
 		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
